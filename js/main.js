d0075582279bb850ff728d9896ea61c4f6f62bc7
ditokapanadze <ditokapanadze0@gmail.com>
2021-02-26 13:52:39 -0800

    landing page tablet (final)

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 2,
+    spaceBetween: 40,
+     
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+      1025:{
+        slidesPerView: 3,
+        spaceBetween: 40,
+      }
+    }
+    
+  });
