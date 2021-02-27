b118a70c9ae14025bf39b02920b432d61df543d3
ditokapanadze <ditokapanadze0@gmail.com>
2021-02-27 12:51:07 -0800

    landing mobile(touch menu added )

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     slidesPerView: 2,
@@ -33,3 +37,72 @@ const swiper = new Swiper('.swiper-container', {
     }
     
   });
+
+  const mobileNav =() =>{
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.mobile-nav-hidden')
+    const deactivate = document.querySelector('header')
+    const worldNews = document.querySelector('.world-news')
+    const bodyFlex = document.querySelector('.body-flex')
+    const swiper = document.querySelector('.swiper-container')
+    const main = document.querySelector('.main-content')
+    
+    
+
+    burger.addEventListener('click', () =>{
+      nav.classList.toggle('mobile-nav-active')
+      deactivate.classList.toggle('deactivate', )
+      // deactivate.classList.toggle('deactivate', )
+      bodyFlex.classList.toggle('deactivate')
+      swiper.classList.toggle('deactivate')
+      main.classList.toggle('deactivate')
+      worldNews.classList.toggle('deactivate')
+
+    })
+     
+  
+
+
+    var min_horizontal_move = 30;
+    var max_vertical_move = 30;
+    var within_ms = 1000;
+ 
+    var start_xPos;
+    var start_yPos;
+    var start_time;
+    function touch_start(event) {
+        start_xPos = event.touches[0].pageX;
+        start_yPos = event.touches[0].pageY;
+        start_time = new Date();
+    }
+ 
+ 
+    function touch_end(event) {
+        var end_xPos = event.changedTouches[0].pageX;
+        var end_yPos = event.changedTouches[0].pageY;
+        var end_time = new Date();
+        let move_x = end_xPos - start_xPos;
+        let move_y = end_yPos - start_yPos;
+        let elapsed_time = end_time - start_time;
+        if (Math.abs(move_x) > min_horizontal_move && Math.abs(move_y) < max_vertical_move && elapsed_time < within_ms) {
+            if (move_x < 0) {
+              swipeMenu.classList.toggle('mobile-nav-hidden')
+            } else {
+                //alert("right");
+            }
+        }
+    }
+ 
+    var swipeMenu = document.querySelector('.mobile-nav-active');
+    content.addEventListener('touchstart', touch_start);
+    content.addEventListener('touchend', touch_end);
+
+
+
+
+  }
+
+
+
+
+  mobileNav()
